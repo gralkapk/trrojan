c4ed9923eb9c795ea9f883251c3143bb2d09caeb
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2026-07-22 20:46:15 +0200

    copy of sphere data to ospray

--- trrojanospray/include/trrojan/ospray/sphere_geometry.h
+++ trrojanospray/include/trrojan/ospray/sphere_geometry.h
@@ -1,17 +1,26 @@
 #pragma once
 
+#include <vector>
+
 #include "trrojan/ospray/object.h"
+
+#include <glm/glm.hpp>
 
 namespace trrojan::ospray {
 class TRROJANOSPRAY_API sphere_geometry final : public object {
 public:
     sphere_geometry();
 
+    explicit sphere_geometry(OSPData positions, OSPData radii);
+
     virtual ~sphere_geometry() = default;
 
     sphere_geometry& setPositions(OSPData positions);
     sphere_geometry& setRadii(OSPData radii);
     sphere_geometry& setGlobalRadius(float radius);
+
+    sphere_geometry& setPositions(const std::vector<glm::vec3>& positions);
+    sphere_geometry& setRadii(const std::vector<float>& radii);
 
 private:
 };
--- trrojanospray/src/sphere_geometry.cpp
+++ trrojanospray/src/sphere_geometry.cpp
@@ -3,6 +3,11 @@
 namespace trrojan::ospray {
 sphere_geometry::sphere_geometry() : object{ospNewGeometry("sphere")} {}
 
+sphere_geometry::sphere_geometry(OSPData positions, OSPData radii) : object{ospNewGeometry("sphere")} {
+    setPositions(positions);
+    setRadii(radii);
+}
+
 sphere_geometry& sphere_geometry::setPositions(OSPData positions) {
     setParam("sphere.position", OSP_DATA, positions);
     return *this;
@@ -15,6 +20,26 @@ sphere_geometry& sphere_geometry::setRadii(OSPData radii) {
 
 sphere_geometry& sphere_geometry::setGlobalRadius(float radius) {
     setParam("radius", OSP_FLOAT, &radius);
+    return *this;
+}
+
+sphere_geometry& sphere_geometry::setPositions(const std::vector<glm::vec3>& positions) {
+    auto tmp_data = ospNewSharedData(positions.data(), OSP_VEC3F, positions.size());
+    auto data = ospNewData(OSP_VEC3F, positions.size());
+    ospCopyData(tmp_data, data);
+    setPositions(data);
+    ospRelease(data);
+    ospRelease(tmp_data);
+    return *this;
+}
+
+sphere_geometry& sphere_geometry::setRadii(const std::vector<float>& radii) {
+    auto tmp_data = ospNewSharedData(radii.data(), OSP_FLOAT, radii.size());
+    auto data = ospNewData(OSP_FLOAT, radii.size());
+    ospCopyData(tmp_data, data);
+    setRadii(data);
+    ospRelease(data);
+    ospRelease(tmp_data);
     return *this;
 }
 } // namespace trrojan::ospray
